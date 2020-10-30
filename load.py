a9ef38710deb65232f8725bec9c4198b2634f969
mathluva <mathluvacoding@gmail.com>
2020-10-30 17:04:00 -0400

    add charts

--- load.py
+++ load.py
@@ -5,11 +5,11 @@ import json
 
 # Create an instance of Flask
 app = Flask(__name__, static_url_path='', static_folder='static')
-app.config["MONGO_URI"] = "mongodb://localhost:27017/crime_db"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/crimedb"
 mongo = PyMongo(app)
 crime = mongo.db.crime
 crime.drop()
-with open('file1.json') as f:
+with open('crimedb.json') as f:
     data = json.load(f)
     for row in data:
         crime.insert_one(row)
--- logic.js
+++ logic.js
@@ -1,3 +1,54 @@
-console.log(data)
+var crimedata = data;
 
-def function()
+    var age = [];
+    var sex = [];
+    var date=[];
+    var loc =[];
+    var dept=[];
+    var charge=[];
+
+    var sex_count = 0;
+    var dept_count = 0;
+    var charge_count =0;
+    var age_count = 0;
+
+// for (var i =0; i<crimedata.length; i++){ 
+ crimedata.forEach((crime) => {
+    age.push(crime.Age);
+    sex.push(crime.Sex);
+    date.push(crime.arrest_date);
+    loc.push(crime.arrest_location);
+    dept.push(crime.agency);
+    charge.push(crime.Charge);   
+ });
+ 
+
+var histogram = [
+    {
+    labels: sex,
+    type: 'pie'
+ }
+];
+
+ var bar = [
+    {
+      x:dept,
+      type:'histogram'
+    }
+  ];
+
+var bubble =[
+    {
+    x: age,
+    y: age,
+    mode: 'markers',
+    marker: {
+      size: [40, 60, 80, 100]
+    }
+    }];
+
+Plotly.newPlot('histogram', histogram);
+Plotly.newPlot('bar', bar);
+Plotly.newPlot('bubble', bubble);
+
+  
